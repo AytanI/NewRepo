bf1afba66d5f34ed6512627fb42c4f1995a3d96f
Aytan <ayten.su@gmail.com>
2022-02-19 13:50:18 -0600

    practice repo

---
+++ src/test/practice.java
@@ -0,0 +1,9 @@
+package test;
+
+public class practice {
+    public static void main(String[] args) {
+        double number = 18;
+        double number1 = 10;
+        System.out.println(number1+number);
+    }
+}
